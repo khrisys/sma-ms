90713de9bd75b87aa37a435e395bb175b6c7e1a1
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-07 21:29:56 +0100

    creation des microservices avec les dependances necessaires en fonction du ms

---
+++ src/test/java/fr/cdrochon/garageservice/GarageServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.garageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GarageServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
