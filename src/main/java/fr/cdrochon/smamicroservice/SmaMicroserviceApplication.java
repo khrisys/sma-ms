19d4b08bddb5e6ea3bc2151a19c5fdd1d95970f5
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-07 19:44:09 +0100

    git init - premier commit

---
+++ src/main/java/fr/cdrochon/smamicroservice/SmaMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.smamicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmaMicroserviceApplication {
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SmaMicroserviceApplication.class, args);
+    }
+    
+}
---
+++ src/test/java/fr/cdrochon/smamicroservice/SmaMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.smamicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmaMicroserviceApplicationTests {
+    
+    @Test
+    void contextLoads() {
+    }
+    
+}
