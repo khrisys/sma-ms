0a8c71b1d53cffd436281a60e360d3cdda8a2531
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 20:26:06 +0100

    var mise final

--- src/main/java/fr/cdrochon/garageservice/controller/GarageRestController.java
+++ src/main/java/fr/cdrochon/garageservice/controller/GarageRestController.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RestController
 public class GarageRestController {
     
-    private GarageRepository garageRepository;
+    private final GarageRepository garageRepository;
     
     public GarageRestController(GarageRepository garageRepository) {
         this.garageRepository = garageRepository;
