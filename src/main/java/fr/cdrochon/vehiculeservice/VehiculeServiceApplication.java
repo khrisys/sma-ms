b09214885dba269298ff6c662bc18f352bc6df66
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 20:28:20 +0100

    creation d'un jeu de données de test pour l'appli avec erreurs pour tester la toelrance aux pannes.

--- src/main/java/fr/cdrochon/vehiculeservice/VehiculeServiceApplication.java
+++ src/main/java/fr/cdrochon/vehiculeservice/VehiculeServiceApplication.java
@@ -1,5 +1,6 @@
 package fr.cdrochon.vehiculeservice;
 
+import fr.cdrochon.vehiculeservice.controller.ClientRestFeign;
 import fr.cdrochon.vehiculeservice.entity.MarqueVehicule;
 import fr.cdrochon.vehiculeservice.entity.TypeVehicule;
 import fr.cdrochon.vehiculeservice.entity.Vehicule;
@@ -7,12 +8,14 @@ import fr.cdrochon.vehiculeservice.repository.VehiculeRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
+@EnableFeignClients
 public class VehiculeServiceApplication {
     
     public static void main(String[] args) {
@@ -20,35 +23,42 @@ public class VehiculeServiceApplication {
     }
     
     @Bean
-    CommandLineRunner commandLineRunner(VehiculeRepository vehiculeRepository) {
+    CommandLineRunner commandLineRunner(VehiculeRepository vehiculeRepository,
+                                        ClientRestFeign clientRestFeign) {
         return args -> {
             
-            List<Vehicule> vehiculeList = List.of(
-                    
-                    Vehicule.builder()
-                            .immatriculationVehicule("PD-158-DZ")
-                            .dateMiseEnCirculationVehicule(LocalDate.now())
-                            .climatisationVehicule(true)
-                            .typeVehicule(TypeVehicule.VOITURE)
-                            .marqueVehicule(MarqueVehicule.BMW)
-                            .build(),
-                    Vehicule.builder()
-                            .immatriculationVehicule("OS-184-PS")
-                            .dateMiseEnCirculationVehicule(LocalDate.of(2020, 12, 14))
-                            .climatisationVehicule(false)
-                            .typeVehicule(TypeVehicule.MOTO)
-                            .marqueVehicule(MarqueVehicule.HARLEY_DAVIDSON)
-                            .build(),
-                    Vehicule.builder()
-                            .immatriculationVehicule("SS-147-DG")
-                            .dateMiseEnCirculationVehicule(LocalDate.of(2023, 8, 1))
-                            .climatisationVehicule(true)
-                            .typeVehicule(TypeVehicule.VOITURE)
-                            .marqueVehicule(MarqueVehicule.AUDI)
-                            .build()
+            clientRestFeign.findEveryClients().forEach(veh -> {
+                Vehicule vehicule1 = Vehicule.builder()
+                                             .immatriculationVehicule("PD-158-DZ")
+                                             .dateMiseEnCirculationVehicule(LocalDate.now())
+                                             .climatisationVehicule(true)
+                                             .typeVehicule(TypeVehicule.VOITURE)
+                                             .marqueVehicule(MarqueVehicule.BMW)
+                                             .clientId(veh.getId())
+                                             .build();
+                Vehicule vehicule2 = Vehicule.builder()
+                                             .immatriculationVehicule("OS-184-PS")
+                                             .dateMiseEnCirculationVehicule(LocalDate.of(2020, 12, 14))
+                                             .climatisationVehicule(false)
+                                             .typeVehicule(TypeVehicule.MOTO)
+                                             .marqueVehicule(MarqueVehicule.HARLEY_DAVIDSON)
+                                             .clientId(veh.getId())
+                                             .build();
+                Vehicule vehicule3 = Vehicule.builder()
+                                             .immatriculationVehicule("SS-147-DG")
+                                             .dateMiseEnCirculationVehicule(LocalDate.of(2023, 8, 1))
+                                             .climatisationVehicule(true)
+                                             .typeVehicule(TypeVehicule.VOITURE)
+                                             .marqueVehicule(MarqueVehicule.AUDI)
+                                             .clientId(veh.getId())
+                                             .build();
+                
+                vehiculeRepository.save(vehicule1);
+                vehiculeRepository.save(vehicule2);
+                vehiculeRepository.save(vehicule3);
+                
+            });
             
-            );
-            vehiculeRepository.saveAll(vehiculeList);
         };
     }
     
--- src/main/java/fr/cdrochon/vehiculeservice/model/AdresseClient.java
+++ src/main/java/fr/cdrochon/vehiculeservice/model/AdresseClient.java
@@ -3,11 +3,12 @@ package fr.cdrochon.vehiculeservice.model;
 import jakarta.persistence.Embeddable;
 import lombok.*;
 
-@Getter @Setter @ToString
+@Getter @Setter @ToString @AllArgsConstructor
 public class AdresseClient {
     
     private String numeroDeRue;
     private String rue;
     private String cp;
     private String ville;
+    
 }
