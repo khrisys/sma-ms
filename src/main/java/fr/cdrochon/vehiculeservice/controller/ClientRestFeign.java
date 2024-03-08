a12f367a22018bfd33c05820d57d502ab8e509b2
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 20:08:24 +0100

    implementation de la tolerance aux pannes avec Resilience4J et CurcuitBreaker pour les interfaces ClientRestFeign et VehiculeRestFeign

--- src/main/java/fr/cdrochon/vehiculeservice/controller/ClientRestFeign.java
+++ src/main/java/fr/cdrochon/vehiculeservice/controller/ClientRestFeign.java
@@ -1,18 +1,58 @@
 package fr.cdrochon.vehiculeservice.controller;
 
+import fr.cdrochon.vehiculeservice.model.AdresseClient;
 import fr.cdrochon.vehiculeservice.model.Client;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
+/**
+ * Communication inter services avec prise en charge de la tolerance aux pannes
+ */
 @FeignClient(name = "CLIENT-SERVICE")
 public interface ClientRestFeign {
     
     @GetMapping("/client/{id}")
+    @CircuitBreaker(name = "clientService", fallbackMethod = "getDefaultClient")
     Client findClientById(@PathVariable Long id);
     
     @GetMapping("/clients")
+    @CircuitBreaker(name = "clientService", fallbackMethod = "getDefaultAllClients")
     List<Client> findEveryClients();
+    
+    /**
+     * Comportement par defaut lorsque le microservice client ne repond pas.
+     *
+     * @param id        id du client
+     * @param exception type exception
+     * @return objet client vide
+     */
+    default Client getDefaultClient(Long id, Exception exception) {
+        AdresseClient adresseClient = new AdresseClient("Non disponible", "Non disponible"
+                , "Non disponible", "Non disponible");
+        
+        Client client = new Client();
+        client.setId(id);
+        client.setNomClient("Non disponible");
+        client.setPrenomClient("Non disponible");
+        client.setTelClient("Non disponible");
+        client.setMailClient("Not available");
+        client.setAdresseClient(adresseClient);
+        System.err.println("Exception default getDefaultCustomer : " + exception.getMessage());
+        return client;
+    }
+    
+    /**
+     * Retourne une liste vide de clients par defaut
+     *
+     * @param exception type exception
+     * @return liste vide de clients
+     */
+    default List<Client> getDefaultAllClients(Exception exception) {
+        System.err.println("Exception default getAllCustomers : " + exception.getMessage());
+        return List.of();
+    }
 }
