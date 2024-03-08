8fce5cdf7e0deb55f589a7fc4d4d432ad0083a65
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 13:03:48 +0100

    dependances du ms discovery

---
+++ src/test/java/fr/cdrochon/discoveryservice/DiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
