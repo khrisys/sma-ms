cf1f08590116c518cd30fbf44a8aea7f5a0c4b93
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 13:04:59 +0100

    squelette et dependances du ms vehicule

---
+++ src/main/java/fr/cdrochon/vehiculeservice/VehiculeServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.cdrochon.vehiculeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiculeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiculeServiceApplication.class, args);
+	}
+
+}
