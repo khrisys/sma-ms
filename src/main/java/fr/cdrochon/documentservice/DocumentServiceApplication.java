1dba70ddebb617b083240c1777453cdbe2554fea
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 17:21:21 +0100

    implementation de l'algo pour la recuperation des infos d'un vehicule pour un document

--- src/main/java/fr/cdrochon/documentservice/DocumentServiceApplication.java
+++ src/main/java/fr/cdrochon/documentservice/DocumentServiceApplication.java
@@ -1,17 +1,20 @@
 package fr.cdrochon.documentservice;
 
+import fr.cdrochon.documentservice.controller.VehiculeRestFeign;
 import fr.cdrochon.documentservice.entity.Document;
 import fr.cdrochon.documentservice.entity.TypeDocument;
 import fr.cdrochon.documentservice.repository.DocumentRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 
 import java.time.LocalDate;
 import java.util.List;
 
 @SpringBootApplication
+@EnableFeignClients
 public class DocumentServiceApplication {
     
     public static void main(String[] args) {
@@ -19,37 +22,42 @@ public class DocumentServiceApplication {
     }
     
     @Bean
-    CommandLineRunner commandLineRunner(DocumentRepository documentRepository) {
+    CommandLineRunner commandLineRunner(DocumentRepository documentRepository,
+                                        VehiculeRestFeign vehiculeRestFeign) {
         return args -> {
-            
-            List<Document> documents = List.of(
-                    
-                    Document.builder()
-                            .nomDocument("devis-00012547")
-                            .titreDocument("rdv pour essieu")
-                            .emetteurDuDocument("garage durand")
-                            .typeDocument(TypeDocument.DEVIS)
-                            .dateCreationDocument(LocalDate.now())
-                            .dateModificationDocument(LocalDate.now())
-                            .build(),
-                    Document.builder()
-                            .nomDocument("facture mr X")
-                            .titreDocument("facture du garage robert")
-                            .emetteurDuDocument("garage robert")
-                            .typeDocument(TypeDocument.FACTURE)
-                            .dateCreationDocument(LocalDate.now())
-                            .dateModificationDocument(LocalDate.now())
-                            .build(),
-                    Document.builder()
-                            .nomDocument("devis sans titre")
-                            .emetteurDuDocument("garage toto")
-                            .typeDocument(TypeDocument.DEVIS)
-                            .dateCreationDocument(LocalDate.now())
-                            .dateModificationDocument(LocalDate.now())
-                            .build()
-            
-            );
-            documentRepository.saveAll(documents);
+            vehiculeRestFeign.findEveryVehicules().forEach(v -> {
+                //FIXME : normalement, il n'y a pas de date de modif d'un doc
+                Document d1 = Document.builder()
+                                      .nomDocument("devis-00012547")
+                                      .titreDocument("rdv pour essieu")
+                                      .emetteurDuDocument("garage durand")
+                                      .typeDocument(TypeDocument.DEVIS)
+                                      .dateCreationDocument(LocalDate.now())
+                                      .dateModificationDocument(LocalDate.now())
+                                      .vehiculeId(v.getId())
+                                      .build();
+                Document d2 = Document.builder()
+                                      .nomDocument("facture mr X")
+                                      .titreDocument("facture du garage robert")
+                                      .emetteurDuDocument("garage robert")
+                                      .typeDocument(TypeDocument.FACTURE)
+                                      .dateCreationDocument(LocalDate.now())
+                                      .dateModificationDocument(LocalDate.now())
+                                      .vehiculeId(v.getId())
+                                      .build();
+                Document d3 = Document.builder()
+                                      .nomDocument("devis sans titre")
+                                      .emetteurDuDocument("garage toto")
+                                      //titre du doc à null
+                                      .typeDocument(TypeDocument.DEVIS)
+                                      .dateCreationDocument(LocalDate.now())
+                                      .dateModificationDocument(LocalDate.now())
+                                      .vehiculeId(v.getId())
+                                      .build();
+                documentRepository.save(d1);
+                documentRepository.save(d2);
+                documentRepository.save(d3);
+            });
         };
     }
 }
--- src/main/java/fr/cdrochon/documentservice/controller/DocumentRestController.java
+++ src/main/java/fr/cdrochon/documentservice/controller/DocumentRestController.java
@@ -1,28 +1,53 @@
 package fr.cdrochon.documentservice.controller;
 
 import fr.cdrochon.documentservice.entity.Document;
+import fr.cdrochon.documentservice.model.Vehicule;
 import fr.cdrochon.documentservice.repository.DocumentRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 @RestController
 public class DocumentRestController {
     
     private final DocumentRepository documentRepository;
+    private final VehiculeRestFeign vehiculeRestFeign;
     
-    public DocumentRestController(DocumentRepository documentRepository) {
+    public DocumentRestController(DocumentRepository documentRepository,
+                                  VehiculeRestFeign vehiculeRestFeign) {
         this.documentRepository = documentRepository;
+        this.vehiculeRestFeign = vehiculeRestFeign;
     }
     
+    /**
+     * Retourne les informations d'un document accompagné des informations du vehicule qu'il concerne
+     *
+     * @param id id de document
+     * @return document
+     */
     @GetMapping("/document/{id}")
-    public Document getDocumentById(@PathVariable Long id){
-        return documentRepository.findById(id).get();
+    public Document getDocumentById(@PathVariable Long id) {
+        Document document = documentRepository.findById(id).get();
+        
+        Vehicule vehicule = vehiculeRestFeign.findVehiculeById(document.getId());
+        document.setVehicule(vehicule);
+        return document;
     }
     
+    /**
+     * Retourne tous les documents concernant un vehicule
+     *
+     * @return liste de documents
+     */
     @GetMapping("/documents")
-    public List<Document> getDocuments(){
-        return documentRepository.findAll();
+    public List<Document> getDocuments() {
+        List<Document> documents = documentRepository.findAll();
+        
+        documents.forEach(doc -> {
+            doc.setVehicule(vehiculeRestFeign.findVehiculeById(doc.getVehiculeId()));
+        });
+        return documents;
     }
 }
