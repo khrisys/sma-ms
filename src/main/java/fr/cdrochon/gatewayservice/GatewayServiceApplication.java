e403908d45d32c08232447cff8d9d14b7ba73823
christian drochon <christian.drochon.auditeur@lecnam.net>
2024-03-08 21:20:52 +0100

    implementation de la gateway

--- src/main/java/fr/cdrochon/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/fr/cdrochon/gatewayservice/GatewayServiceApplication.java
@@ -2,12 +2,29 @@ package fr.cdrochon.gatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayServiceApplication {
     
     public static void main(String[] args) {
         SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+    
+    /**
+     * Configuration dynamique des routes
+     *
+     * @param client
+     * @param discoveryLocatorProperties
+     * @return
+     */
+    @Bean
+    DiscoveryClientRouteDefinitionLocator locator(ReactiveDiscoveryClient client,
+                                                  DiscoveryLocatorProperties discoveryLocatorProperties) {
+        return new DiscoveryClientRouteDefinitionLocator(client, discoveryLocatorProperties);
     }
     
 }
